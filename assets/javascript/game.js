07d13524ba3104da36f1f32c602f6b9d39010338
Molly Zurheide <you@example.com>
2018-04-06 18:48:57 -0400

    couple of quick changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,19 @@
 
 var wins = 0; //only on load
 var losses = 0; //only on load
-var targetScore = Math.floor(Math.random()*(120-19+1)+19); //to generate target score  (***WORKS***)
+var targetScore = Math.floor(Math.random()*(120-19+1)+19);
 var userScore = 0;
 var crystals = $("#crystals");
-//var crystalValue;   // (***NOT WORKING***)
-var crystalValueIndex = Math.floor(Math.random()*(12-1+1)+1);   //((***WORKS***))
-//var crystalImage;
+var crystalValueIndex = Math.floor(Math.random()*(12-1+1)+1);
 var crystalImageOptions = [ //array of crystal images
     "assets/images/crystal1.jpg",
     "assets/images/crystal2.jpg",
     "assets/images/crystal3.jpg",
     "assets/images/crystal4.jpg"
 ];
-var crystalImageIndex = Math.floor(Math.random()*4+1); //to select random image from the 4 options (***WORKS***)
+var crystalImageIndex = Math.floor(Math.random()*4+1); //to select random image from the 4 options
 
-function startGame() { //start game                  ***WORKS***
+function startGame() { //load 4 crystal images and their values
     $("#target-number").text(targetScore); //print target score to corresponding div
     for (var i = 0; i < 4; i++) {  //run for-loop function 4 times (once for each crystal)
         var crystalImage = $("<img>"); //create image tag for crystal
@@ -27,18 +25,16 @@ function startGame() { //start game                  ***WORKS***
     }
 }
 
-
-
 ////////////////////////////////////////////////////
 
-//CALLING MAIN GAME FUNCTION
+//CALLING MAIN GAME FUNCTIONS
 
-$(document).ready(function() {   //(***WORKS***)
+$(document).ready(function() {
     startGame();
     //console.log("testing"); (***WORKS***)
     crystals.on("click", ".crystal-image", function() {   //THIS DOESN'T WORK!!!!!!!!!!!!!!!!!!!!!!!!!
         var crystalValue = ($(this).attr("data-crystalvalue")); //grab value of each crystal
-        //console.log("testing"); (***DOESN'T WORK!***)
+        //console.log("testing");   //THIS DOESN'T WORK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
         crystalValue = parseInt(crystalValue); //convert from string to integer
         userScore += crytalValue; //add crystal's value to user's score each time any crystal is clicked
         $("#scoreboard").text(userScore); //print updated user score
